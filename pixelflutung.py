6eb5a35d1b62f27bbf64d0806f69963c6e2b760b
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2016-07-08 00:46:17 +0200

    numpy + paho mqtt demo

--- pixelflutung.py
+++ pixelflutung.py
@@ -1,4 +1,5 @@
 import struct
+import numpy as np
 import os, sys
 import time
 import paho.mqtt.client as mqtt
@@ -13,16 +14,31 @@ client.connect("mqtt", 1883, 60)
 
 #client.publish('cracki/esp-devboard/gameoflife', '254')
 
-bitmap = bytearray([0] * 128)
+bitmap = np.zeros((16, 64), dtype=np.uint8)
 
-for x in xrange(256):
-	for k in xrange(128):
-		bitmap[k] = (k + x) % 256
-	client.publish('cracki/esp-devboard/bitmap', bitmap)
-	time.sleep(delay)
+def publish_bitmap():
+	client.publish('cracki/esp-devboard/bitmap', bytearray(np.packbits(bitmap).tobytes()))
+
+if 1:
+	x = 0
+	y = 0
+	i = 0
+	while True:
+	    i += 1
+	    if i % 2 == 0:
+	            x += 1
+	            bitmap[:] = 0
+	            bitmap[:,x%64] = 1
+	            publish_bitmap()
+	    else:
+	            y += 1
+	            bitmap[:] = 0
+	            bitmap[y%16,:] = 1
+	            publish_bitmap()
+	    time.sleep(delay)
 
 if 0:
 	for y in xrange(16):
 		for x in xrange(64):
-			client.publish('cracki/esp-devboard/pixelflut', 'PX {} {} 0'.format(x, y))
+			client.publish('cracki/esp-devboard/pixelflut', 'PX {} {} 1'.format(x, y))
 			time.sleep(delay)
